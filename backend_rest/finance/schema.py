7d8fceacb38ea39d2a33b94220ba3856ce127f9b
ezrankayamba <ezrankayamba@gmail.com>
2021-01-31 23:34:50 +0300

    Removed page size limit

--- backend_rest/finance/schema.py
+++ backend_rest/finance/schema.py
@@ -78,17 +78,15 @@ class Query(object):
     tithe_summary = graphene.List(TitheSummaryType)
 
     @login_required
-    def resolve_entries(self, info,  page_size, page_no=1, **kwargs):
+    def resolve_entries(self, info, page_no=1, page_size=DEFAULT_PAGE_SIZE, **kwargs):
         print(kwargs)
         params = utils.params_entry_filter(kwargs)
         print(params)
-        if page_size:
-            start = ((page_no - 1) * page_size)
-            to = page_no * page_size
-            print(f'Start: {start}, To: {to}')
-            qs = models.Entry.objects.select_related('entity').filter(**params)[start:to]
-        else:
-            qs = models.Entry.objects.select_related('entity').filter(**params)
+        start = ((page_no - 1) * page_size)
+        to = page_no * page_size
+        print(f'Start: {start}, To: {to}')
+        qs = models.Entry.objects.select_related(
+            'entity').filter(**params)[start:to]
 
         return qs
 
@@ -108,16 +106,18 @@ class Query(object):
         return qs
 
     @login_required
-    def resolve_entities(self, info, page_no=1, page_size=DEFAULT_PAGE_SIZE,  **kwargs):
+    def resolve_entities(self, info,  page_size,  page_no=1, **kwargs):
         print(kwargs)
         params = {}
         # params = utils.params_entry_filter(kwargs)
         # print(params)
-        start = ((page_no - 1) * page_size)
-        to = page_no * page_size
-        print(f'Start: {start}, To: {to}')
-        qs = models.Entity.objects.filter(**params)[start:to]
-
+        if page_size:
+            start = ((page_no - 1) * page_size)
+            to = page_no * page_size
+            print(f'Start: {start}, To: {to}')
+            qs = models.Entity.objects.filter(**params)[start:to]
+        else:
+            qs = models.Entity.objects.filter(**params)
         return qs
 
     @login_required
